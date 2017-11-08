f3a6e41f5a8037e27969af16acecbce22f21a399
Lena Berglund <31613596+lebe1006@users.noreply.github.com>
2017-11-08 14:34:40 +0100

    ny data-klass

---
+++ GruppG/Data/PersonData.cs
@@ -0,0 +1,19 @@
+﻿using GruppG.Models.db;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GruppG.Data
+{
+    public class PersonData
+    {
+        private U4Entities db = new U4Entities();
+        //Koppla ihop inloggad person med person i databasen.
+        public Person GetPersonById(int id)
+        {
+            var person = db.Person.FirstOrDefault(x => x.Id == id);
+            return person;
+        }
+    }
+}
