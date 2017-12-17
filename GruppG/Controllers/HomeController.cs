788e28b2ae64fe186db5fd5b431fdb5f65ed180e
nada1600 <nada1600@student.miun.se>
2017-12-17 17:37:13 +0100

    Fortsätter imorgon med datumparametern.

--- GruppG/Controllers/HomeController.cs
+++ GruppG/Controllers/HomeController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-//**NYTT**
 //Lagt till dessa för att komma åt db
 ///using System.Data;
 using System.Data.Entity;
@@ -23,6 +22,8 @@ namespace GruppG.Controllers
         private ProgramData pd = new ProgramData();
         private Person person = new Person();
         private ChannelData channeldate = new ChannelData();
+
+        //Dessa kommer att tas bort när adtumparametern fungerar
         DateTime yesterday = DateTime.Today.Date.AddDays(-1);
         DateTime today = DateTime.Today;
         DateTime tomorrow = DateTime.Today.Date.AddDays(2);
@@ -34,6 +35,7 @@ namespace GruppG.Controllers
         DateTime tuesday = Convert.ToDateTime("11/14/2017");
         DateTime wednesday = Convert.ToDateTime("11/15/2017");
         DateTime thursday = Convert.ToDateTime("11/16/2017");
+        //-----------------------------------------------------------
         
         //GAMLA INDEX
         //public ActionResult Index()
@@ -57,37 +59,6 @@ namespace GruppG.Controllers
             
         }
 
-
-
-        public ActionResult Friday()
-        {
-            return View();
-        }
-        public ActionResult Saturday()
-        {
-            return View(); 
-        }
-        public ActionResult Sunday()
-        {
-            return View();
-        }
-        public ActionResult Monday()
-        {
-            return View();
-        }
-        public ActionResult Tuesday()
-        {
-            return View();
-        }
-        public ActionResult Wednesday()
-        {
-            return View();
-        }
-        public ActionResult Thursday()
-        {
-            return View();
-        }
-        //------------------------------------------------
 
         // GET: ProgramsCategory/Details/
         /*Den här action-metoden kan vi använda för att visa detaljer om programmen 
@@ -148,13 +119,146 @@ namespace GruppG.Controllers
 
         //------------------------------------------------------------
 
-        //Nytt 16/12
+        //Nytt 16/12 - Visar specifik kanals program med hjälp av parametern "channel".
         public ActionResult _Channel (int channel, DateTime date)
         {
             var p = pd.GetChannel(channel, date);
             return PartialView(p);
         }
         //------------------------------------------------------------
+        
+
+        // Hämtar programdetaljer
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Program program = db.Program.Find(id);
+            if (program == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView(program);
+        }
+        
+
+        public ActionResult Datum(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Program program = db.Program.Find(id);
+            if (program == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView(program);
+        }
+
+
+        //Kontakt
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Kontakta oss:";
+
+            return View();
+        }
+
+
+
+
+
+
+        //[HttpPost]
+        //public ActionResult Login(LoginVM model, string ReturnUrl)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        //Skapar en log in cookie som är persistent. Den försvinner när browsern stängs.
+        //        FormsAuthentication.SetAuthCookie(model.UserName, false);
+        //        //FormsAuthentication.SetAuthCookie(model.Password, false);
+        //        return Redirect(ReturnUrl);
+
+
+        //    }
+
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //public ActionResult Login(GruppG.Models.db.Person pers)
+        //{
+        //    using (U4Entities u4 = new U4Entities())
+        //    {
+        //        var ud = u4.Person.Where(x => x.UserName == pers.UserName && x.Password == pers.Password).FirstOrDefault();
+        //        if (ud == null)
+        //        {
+        //            pers.Password = "Du har angett fel användarnamn eller lösenord";
+        //            return View("Index", pers);
+        //        }
+        //    }
+
+
+
+        //    return View();
+
+        //if (ModelState.IsValid)
+        //{
+        //Skapar en log in cookie som är persistent. Den försvinner när browsern stängs.
+        //FormsAuthentication.SetAuthCookie(model.UserName, false);
+        //FormsAuthentication.SetAuthCookie(model.Password, false);
+        //pd.CheckUserCreadentials();
+
+        //}
+
+        //    return View();
+        //}
+
+
+
+
+
+
+        //----------------------------------------------------------
+        //Dessa kommer tas bort så fort datum-parametern fungerar! :)
+
+        public ActionResult Friday()
+        {
+            return View();
+        }
+        public ActionResult Saturday()
+        {
+            return View();
+        }
+        public ActionResult Sunday()
+        {
+            return View();
+        }
+        public ActionResult Monday()
+        {
+            return View();
+        }
+        public ActionResult Tuesday()
+        {
+            return View();
+        }
+        public ActionResult Wednesday()
+        {
+            return View();
+        }
+        public ActionResult Thursday()
+        {
+            return View();
+        }
+        //------------------------------------------------
+
+
+
+
+
 
         //PartialViews ref: https://www.youtube.com/watch?v=SABg7RyjX-4
         public ActionResult SVT1()
@@ -266,7 +370,7 @@ namespace GruppG.Controllers
 
             U4Entities pwdb = new U4Entities();
 
-            
+
             var p = pwdb.Program.Where(Program => Program.Chanel == 3).Where(q => q.Starttime == friday);
             //Program SVT1 = pwdb.Program.Where(Program => Program.Chanel == 1);
 
@@ -331,7 +435,7 @@ namespace GruppG.Controllers
         {
             U4Entities pwdb = new U4Entities();
             var p = pwdb.Program.Where(Program => Program.Chanel == 4).Where(q => q.Starttime == friday);
-          
+
             return PartialView(p.ToList());
         }
         public ActionResult TV4saturday()
@@ -388,7 +492,7 @@ namespace GruppG.Controllers
         {
             U4Entities pwdb = new U4Entities();
             var p = pwdb.Program.Where(Program => Program.Chanel == 5).Where(q => q.Starttime == today);
-          
+
             return PartialView(p.ToList());
         }
         public ActionResult Kanal5saturday()
@@ -430,106 +534,9 @@ namespace GruppG.Controllers
         {
             U4Entities pwdb = new U4Entities();
             var p = pwdb.Program.Where(Program => Program.Chanel == 5).Where(q => q.Starttime == wednesday);
-           
+
             return PartialView(p.ToList());
         }
-        public ActionResult NoProgram()
-        {
-            return PartialView();
-        }
-        
 
-
-        // Hämta programmets details
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Program program = db.Program.Find(id);
-            if (program == null)
-            {
-                return HttpNotFound();
-            }
-            return PartialView(program);
-        }
-        
-
-        public ActionResult Datum(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Program program = db.Program.Find(id);
-            if (program == null)
-            {
-                return HttpNotFound();
-            }
-            return PartialView(program);
-        }
-
-
-
-
-
-
-
-      
-
-        //[HttpPost]
-        //public ActionResult Login(LoginVM model, string ReturnUrl)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        //Skapar en log in cookie som är persistent. Den försvinner när browsern stängs.
-        //        FormsAuthentication.SetAuthCookie(model.UserName, false);
-        //        //FormsAuthentication.SetAuthCookie(model.Password, false);
-        //        return Redirect(ReturnUrl);
-
-
-        //    }
-
-        //    return View();
-        //}
-
-        //[HttpPost]
-        //public ActionResult Login(GruppG.Models.db.Person pers)
-        //{
-        //    using (U4Entities u4 = new U4Entities())
-        //    {
-        //        var ud = u4.Person.Where(x => x.UserName == pers.UserName && x.Password == pers.Password).FirstOrDefault();
-        //        if (ud == null)
-        //        {
-        //            pers.Password = "Du har angett fel användarnamn eller lösenord";
-        //            return View("Index", pers);
-        //        }
-        //    }
-
-
-
-        //    return View();
-
-        //if (ModelState.IsValid)
-        //{
-        //Skapar en log in cookie som är persistent. Den försvinner när browsern stängs.
-        //FormsAuthentication.SetAuthCookie(model.UserName, false);
-        //FormsAuthentication.SetAuthCookie(model.Password, false);
-        //pd.CheckUserCreadentials();
-
-        //}
-
-        //    return View();
-        //}
-
-       
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Kontakta oss:";
-
-            return View(); 
-        }
     }
 }
