94545a3f0ee611ed3a4823bd39b621f35e8ff1f7
Ägaren <garen@GAREN-DATOR>
2018-07-25 08:02:19 +0200

    VM-uppdatering

--- GruppG/Models/ViewModels/ProgramChannelVM.cs
+++ GruppG/Models/ViewModels/ProgramChannelVM.cs
@@ -3,13 +3,19 @@ using GruppG.Models.db;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.DynamicData;
 
 namespace GruppG.Models.ViewModels
 {
     public class ProgramChannelVM
     {
+        private U4Entities db = new U4Entities();
+
         public IEnumerable<Program> Program { get; set; }
-       
+
+        List<string> Channels = new List<string>();
         
+   
+	
     }
 }
