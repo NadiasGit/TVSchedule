70e52b7e3d7f25324fcf4e3778b3c55c07a41e75
nada1600 <nada1600@student.miun.se>
2018-08-06 12:51:15 +0200

    forts. förkortning av datum...

--- GruppG/Models/ViewModels/ProgramChannelVM.cs
+++ GruppG/Models/ViewModels/ProgramChannelVM.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.DynamicData;
+using System.ComponentModel.DataAnnotations;
 
 namespace GruppG.Models.ViewModels
 {
@@ -22,15 +23,18 @@ namespace GruppG.Models.ViewModels
         public IEnumerable<Program> Programs { get; set; }
         public Program Program { get; set; }
         public List<DateTime> Dates { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime Today { get; set; }
 
         public ProgramChannelVM()
         {
             //Om vi/du vill ha aktuella datum
-            //Today = DateTime.Now;
+            Today = DateTime.Today;
             //Om vi vill se hårdkodade programmen
             //DateTime today = Convert.ToDateTime("2017-11-09");
-            Today = Convert.ToDateTime("2017-11-09").Date;
+            //Today = Convert.ToDateTime("2017-11-09").Date;
+            //Today = new DateTime(2017, 11, 09);
 
             Dates = new List<DateTime>();
             Dates.Add(Today);
@@ -40,15 +44,6 @@ namespace GruppG.Models.ViewModels
             Dates.Add(Today.AddDays(4));
             Dates.Add(Today.AddDays(5));
             Dates.Add(Today.AddDays(6));
-
-
-            //Dates.Add(Today.ToShortDateString());
-            //Dates.Add(Today.AddDays(1).ToShortDateString());
-            //Dates.Add(Today.AddDays(2).ToShortDateString());
-            //Dates.Add(Today.AddDays(3).ToShortDateString());
-            //Dates.Add(Today.AddDays(4).ToShortDateString());
-            //Dates.Add(Today.AddDays(5).ToShortDateString());
-            //Dates.Add(Today.AddDays(6).ToShortDateString());
 
         }
 
