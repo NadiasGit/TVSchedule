d2bf6db3a6b8aeb6def6d8dd549b005ac860ca03
Ägaren <garen@GAREN-DATOR>
2018-08-29 07:47:30 +0200

    Mini-test

--- GruppG/Security/AuthorizeRolesAttribute.cs
+++ GruppG/Security/AuthorizeRolesAttribute.cs
@@ -27,6 +27,22 @@ namespace GruppG.Security
             foreach (var roles in userAssignedRole)
             {
                 authorize = pd.UserInRole(httpContext.User.Identity.Name, roles);
+                //Testkod:
+                //var test = this.pd.UserInRole(httpContext.User.Identity.Name, roles);
+                //var user = httpContext.User;
+                //var allowedRoles = this.Roles.Split(',');
+                //if (!user.Identity.IsAuthenticated)
+                //{
+                //    return false;
+                //}
+                //else
+                //{
+                //    authorize = true;
+                //}
+                //var test = this.pd.UserInRole(httpContext.User.Identity.Name, roles);
+
+                //------
+
                 if (authorize)
                     return authorize;
             }
