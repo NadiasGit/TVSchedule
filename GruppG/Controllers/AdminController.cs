71791e198c1cb8dac3a86f9742a2553faa4d42d2
nada1600 <nada1600@student.miun.se>
2018-08-30 19:16:01 +0200

    Städat i admin controller & vy.

--- GruppG/Controllers/AdminController.cs
+++ GruppG/Controllers/AdminController.cs
@@ -5,12 +5,13 @@ using GruppG.Security;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
 namespace GruppG.Controllers
 {
-    [AuthorizeRoles("Admin")] //<= Ska användas för att inte komma åt sidan om man inte är inloggad
+    [AuthorizeRoles("Admin")] //<= Only admins will have access to these views/functions.
     public class AdminController : Controller
     {
         private ProgramData pd = new ProgramData();
@@ -31,19 +32,17 @@ namespace GruppG.Controllers
         //Program Details
         public ActionResult ProgramDetails(int id)
         {
-            //Lägg till ett felmeddelande/felhantering om program-id saknas
             var progDetails = db.Program.Single(e => e.Id == id);
             ViewBag.Message = pd.PuffName(progDetails.Puff);
-            //return RedirectToAction("ProgramDetails", "Home");
-            return View(progDetails);
+            return View(progDetails);  
         }
 
 
         //Create puff
-            //For future tasks...
+        //For future tasks...
 
 
-        //Edit PUFF
+        //Add PUFF
         public ActionResult Edit(int? id)
         {
             ViewBag.Message = "Max antal puffar är 3!";
@@ -55,7 +54,6 @@ namespace GruppG.Controllers
             }
             else if (id == null)
             {
-                //return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
                 return ViewBag.Message=("Något blev fel!");
             }
             else
@@ -64,10 +62,8 @@ namespace GruppG.Controllers
                 TempData["messageSuccess"] = "Puffen är registrerad.";
                 ViewBag.Message = pd.PuffName(progEdit.Puff);
                 return View(progEdit);
-            }
-            
+            }            
         }
-
 
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -75,39 +71,27 @@ namespace GruppG.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Edit(int id)
         {
-                //Index -> program i modelstate-view
-                //Program som inp. 
-            
-                Program puffToEdit = db.Program.Find(id);
-                //if (puffToEdit == null)
-                //{
-                //    return HttpNotFound();
-                //}
-
-                
-                if (!ModelState.IsValid)
-                {
-                    return View("Index");
-                }
-                else
-            puffToEdit.Puff = 1;
-            db.SaveChanges();
-            
-            //var newPuff = pd.PuffPrograms().Where(p => p.Puff == 1);
-            return RedirectToAction("Index");
-
-            
+            Program puffToEdit = db.Program.Find(id);
+            if (puffToEdit == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                puffToEdit.Puff = 1;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
         }        
         
 
-        //Delete puffar
+        //Delete PUFF
         public ActionResult Delete(int? id)
         {
             Program puffDelete = db.Program.Find(id);
             if (id == null)
             {
-                //return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-                return ViewBag.Message = ("Finns inget att radera...");
+                return ViewBag.Message = ("Finns inget att radera.");
             }
             else
             {
@@ -129,45 +113,8 @@ namespace GruppG.Controllers
 
             programToRemove.Puff = 0;
             db.SaveChanges();
-            
-            //var puff = db.Program.Select(p => p.Puff == 1 );
-            //db.Program.Remove(program);
-            //db.SaveChanges();
-            //var puffList = pd.PuffPrograms().Where(p => p.Puff == 1);
             return RedirectToAction("Index");
         }
-
-
-
-
-
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Index(int id, Program program)
-        //{
-        //    Program puffToEdit = db.Program.Find(id);
-        //    if (puffToEdit == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-
-        //    else
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return View(program);
-        //    }
-        //    puffToEdit.Puff = program.Puff;
-        //    puffToEdit.Puff = 0;
-        //    var puffToRemove = pd.PuffPrograms().Where(i => i.Puff != 1).ToList();
-
-
-        //    db.SaveChanges();
-
-        //    //var newPuff = pd.PuffPrograms().Where(p => p.Puff == program.Puff);
-        //    return RedirectToAction("Index");
-
-
-        //}
 
 
 
